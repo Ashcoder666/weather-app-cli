d9a5f2450622a05343e6d545be6d989d5af4ab89
Ashcoder666 <ashirnbasheer@gmail.com>
2023-12-21 11:44:33 +0530

    working

---
+++ index.py
@@ -0,0 +1,28 @@
+#enter input
+#show weather of that location from weather api
+
+
+import requests
+
+
+
+input_val = ""
+api_host = "weatherapi-com.p.rapidapi.com"
+api_key = "c256ecdf6emshcedc5242d337c13p19a862jsnee79af877651"
+while input_val != "exit":
+    input_val= input("enter location : \n")
+    url = f"https://weatherapi-com.p.rapidapi.com/current.json?q={input_val}"
+    headers = {
+          "X-RapidAPI-Host": api_host,
+        "X-RapidAPI-Key": api_key
+    }
+    response = requests.get(url,headers=headers)
+    
+    if response.status_code == 200:
+        print(response.json())
+    elif 400:
+        print("invalid location")
+    else:
+        print("server error")
+    
+
